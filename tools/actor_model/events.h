df5febdb68f2136022014a9a266af6d038c33a2c
Никита Манжиков <53493546+kito4@users.noreply.github.com>
2025-05-28 19:08:24 +0300

    Update events.h

--- tools/actor_model/events.h
+++ tools/actor_model/events.h
@@ -1,6 +1,13 @@
-#include <library/cpp/actors/core/event_local.h>
+#pragma once
+
 #include <library/cpp/actors/core/events.h>
 
-struct TEvents {
-    // Вам нужно самостоятельно сюда добавить все необходимые events в NActors::TEvents::ES_PRIVATE
-};
+namespace TEvents {
+    struct TEvDone : NActors::TEventLocal<TEvDone, 1> {};
+
+    struct TEvWriteValueRequest : NActors::TEventLocal<TEvWriteValueRequest, 2> {
+        int64_t Value;
+        TEvWriteValueRequest(int64_t value) : Value(value) {}
+    };
+}
+
